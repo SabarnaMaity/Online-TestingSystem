3186da42497e775c0450b2508997226fcd015f43
SabarnaMaity <sabarnamaity2002@gmail.com>
2024-07-16 22:03:04 +0530

    Online Testing System

---
+++ myproject/OTS/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from OTS.models import *
+admin.site.register(Candidate)
+admin.site.register(Question)
+admin.site.register(Result)
